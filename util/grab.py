e2b9e8728de016b4fdc3d9e3121490dc8c077101
justusadam <development@justusadam.com>
2015-04-13 08:16:28 +0200

    made python script configurable

--- util/grab.py
+++ util/grab.py
@@ -6,7 +6,8 @@ import sys
 import os
 import json
 
-grab_table_regex = re.compile('<h1>4\. Semester</h1>.*?<table>(.*?)</table>', flags=re.DOTALL)
+def grab_table_regex(semester):
+    return re.compile('<h1>{}\. Semester</h1>.*?<table>(.*?)</table>'.format(semester), flags=re.DOTALL)
 tr_regex = re.compile('<tr>(.*?)</tr>', flags=re.DOTALL)
 td_regex = re.compile('<td>(.*?)</td>', flags=re.DOTALL)
 a_regex = re.compile('<a .*?">(.*?)</a>', flags=re.DOTALL)
@@ -24,34 +25,28 @@ days = {
 }
 
 
-def apply_each(func, vals):
-    return tuple(func(v) for v in vals)
+def splitter(s, a, b):
+    for s1 in s.split(a):
+        for s2 in s1.split(b):
+            s2 = s2.replace(' ', '')
+            if s2:
+                yield s2
 
 
-def main():
-    file = sys.argv[1]
+def extract_data(string, semester):
 
-    content = urlopen('http://web.inf.tu-dresden.de/Fak/ss/15/studiengang/studiengang_inf_bach.html').read().decode()
-
-    table = grab_table_regex.search(content).group(1)
+    regex1 = grab_table_regex(semester)
+    table = regex1.search(string).group(1)
 
     _, *lessons = [a.group(1) for a in list(re.finditer(tr_regex, table))]
 
-    lessons = list(
+    lessons = (
         list(
             a.group(1)
             for a in list(re.finditer(td_regex, l))
         )
         for l in lessons
     )
-
-    def splitter(s, a, b):
-        for s1 in s.split(a):
-            for s2 in s1.split(b):
-                s2 = s2.replace(' ', '')
-                if s2:
-                    yield s2
-
 
     def l():
         for a in lessons:
@@ -70,7 +65,32 @@ def main():
                         slot=int(slot.replace('.', '').replace(' ', ''))
                     )
 
-    lessons = list(l())
+    return list(l())
+
+
+def apply_each(func, vals):
+    return tuple(func(v) for v in vals)
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '-o', '--output', type=str, default='out.json'
+    )
+    parser.add_argument(
+        'semester', type=int
+    )
+
+    args = parser.parse_args()
+
+    file = args.output
+
+    content = urlopen('http://web.inf.tu-dresden.de/Fak/ss/15/studiengang/studiengang_inf_bach.html').read().decode()
+
+    lessons = extract_data(content, args.semester)
 
     if os.path.exists(file):
         mode = 'w'
