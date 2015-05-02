306b0ad6f2a39bf1a0ac33563a039328c5507361
justusadam <development@justusadam.com>
2015-05-02 11:51:03 +0200

    simplifying data structures

--- src/Calculator/Scale.hs
+++ src/Calculator/Scale.hs
@@ -17,19 +17,19 @@ module Calculator.Scale (
   calcMaps
 ) where
 
-import           Calculator.Solver (Lesson (..), time, timeslot)
-import           Control.Monad
-import           Data.Data         (Data)
-import           Data.List         as List (mapAccumL)
-import qualified Data.Map          as Map (Map, empty, findWithDefault, insert,
-                                           insertWith, lookup, update)
-import           Data.Tuple        (swap)
-import           Data.Typeable     (Typeable)
-import Control.Monad.Trans.State
+import           Calculator.Solver         (Lesson (..), time, timeslot)
+import           Control.Monad.Trans.State (State, put, get, runState)
+import           Data.Data                 (Data)
+import           Data.List                 as List (mapAccumL)
+import qualified Data.Map                  as Map (Map, empty, findWithDefault,
+                                                   insert, insertWith, lookup,
+                                                   update)
+import           Data.Tuple                (swap)
+import           Data.Typeable             (Typeable)
 
 
 -- |The scope and target a 'Rule' whishes to influence
-data Target         = Slot Int | Day Int | Cell Int Int deriving (Show, Typeable, Data)
+data Target         = Slot Int | Day Int | Cell Int Int deriving (Show, Typeable, Data, Ord, Eq)
 -- |Weight increase by 'severity' for all 'Lesson's in target
 data Rule           = Rule {target :: Target, severity :: Int} deriving (Show, Typeable, Data)
 -- |Dynamic rule with only one condition
@@ -37,53 +37,46 @@ data SimpleDynRule  = SimpleDynRule {sDynTarget :: Target, sDynSeverity :: Int} 
 
 
 -- |Type alias for more expressive function signature
-type DayWeightMap   = Map.Map Int Int
--- |Type alias for more expressive function signature
-type SlotWeightMap  = Map.Map Int Int
--- |Type alias for more expressive function signature
-type CellWeighMap   = Map.Map (Int, Int) Int
--- |Type alias for more expressive function signature
-type WeightMapTuple = (SlotWeightMap, DayWeightMap, CellWeighMap)
+type WeightMap   = Map.Map Target Int
 -- |Type alias for structure holding the dynamic rules
 type DynRuleTuple   = (Map.Map Int [SimpleDynRule],
                        Map.Map Int [SimpleDynRule],
                        Map.Map (Int, Int) [SimpleDynRule])
 
 
+-- instance Ord Target where
+
+
 -- |Scaffolding of a dynamic rule
 class DynamicRule a where
-  trigger          :: Lesson s -> WeightMapTuple -> a -> (WeightMapTuple, a)
+  trigger          :: Lesson s -> WeightMap -> a -> (WeightMap, a)
   getTriggerTarget :: a -> [Target]
 
 
 instance DynamicRule SimpleDynRule where
-  trigger inserted (ms, md, mc) (SimpleDynRule target sev) = (
-
-    case target of
-      Slot slot   ->  (ms, Map.insertWith (+) slot sev md, mc)
-      Day  day    ->  (Map.insertWith (+) day sev ms, md, mc)
-      Cell c1 c2  ->  (ms, md, Map.insertWith (+) (c1, c2) sev mc)
-
-    , SimpleDynRule target sev)
+  trigger inserted wMap (SimpleDynRule target sev) =
+    (Map.insertWith (+) target sev wMap, SimpleDynRule target sev)
 
   getTriggerTarget = return.sDynTarget
 
 
 -- |Recalculate the lesson weight tuple as a result of dynamic rules
-reCalcMaps :: WeightMapTuple -> Lesson s -> DynRuleTuple -> (DynRuleTuple, WeightMapTuple)
-reCalcMaps wmt l (rs, rd, rc) = runState (do
-  rsn <- reCalcHelper l timeslot rs
-  rdn <- reCalcHelper l day rd
-  rcn <- reCalcHelper l time rc
-  return (rsn, rdn, rcn)) wmt
+reCalcMaps :: WeightMap -> Lesson s -> DynRuleTuple -> (DynRuleTuple, WeightMap)
+reCalcMaps weightMap lesson (slotRules, dayRules, cellRules) = runState (do
+
+  newSlotRules  <- reCalcHelper lesson timeslot slotRules
+  newDayRules   <- reCalcHelper lesson day      dayRules
+  newCellRules  <- reCalcHelper lesson time     cellRules
+
+  return (newSlotRules, newDayRules, newCellRules)) weightMap
 
 
 reCalcHelper :: Ord k
     => Lesson s
     -> (Lesson s -> k)
     -> Map.Map k [SimpleDynRule]
-    -> State WeightMapTuple (Map.Map k [SimpleDynRule])
-reC inserted accessor rMap =
+    -> State WeightMap (Map.Map k [SimpleDynRule])
+reCalcHelper inserted accessor rMap =
   case Map.lookup (accessor inserted) rMap of
     Nothing     -> return rMap
     Just rules  -> do
@@ -120,37 +113,30 @@ weigh rs = map (weighOne (calcMaps rs))
   Weighs a single 'Lesson', but instead of 'Rule's expects a
   'Tuple' of weight increase maps.
 -}
-weighOne :: WeightMapTuple -> Lesson s -> Lesson s
-weighOne (ms, md, mc) l =
-  l {
-      weight =
-        weight l
-        + Map.findWithDefault 0 (timeslot l) ms
-        + Map.findWithDefault 0 (day l) md
-        + Map.findWithDefault 0 (time l) mc
-    }
+weighOne :: WeightMap -> Lesson s -> Lesson s
+weighOne wm l =
+  l {weight = weight l + allTargeting l wm}
+
+
+allTargeting :: Lesson s -> WeightMap -> Int
+allTargeting l = pure (\a b c -> a + b + c)
+  <*> Map.findWithDefault 0 (Slot $ timeslot l)
+  <*> Map.findWithDefault 0 (Day $ day l)
+  <*> Map.findWithDefault 0 ((uncurry Cell) $ time l)
+
 
 
 {-|
   Contruct a 'Tuple' of /scope -> weight increase/ maps for more efficient
   weighing afterwards
 -}
-calcMaps :: [Rule] -> WeightMapTuple
-calcMaps = (`calcMapsStep` (Map.empty, Map.empty, Map.empty))
+calcMaps :: [Rule] -> WeightMap
+calcMaps = (`calcMapsStep` Map.empty)
 
 
 {-|
   Recursive step for the actual calculation done by 'calcMaps'
 -}
-calcMapsStep :: [Rule] -> WeightMapTuple -> WeightMapTuple
-calcMapsStep [] mt                          = mt
-calcMapsStep (Rule t sev :xs) (ms, md, mc)  = calcMapsStep xs newMaps
-
-  where
-    increase :: Ord k => k -> Map.Map k Int -> Map.Map k Int
-    increase target = Map.insertWith (+) target sev
-
-    newMaps = case t of
-                Slot s      -> (increase s ms, md, mc)
-                Day d       -> (ms, increase d md, mc)
-                Cell c1 c2  -> (ms, md, increase (c1, c2) mc)
+calcMapsStep :: [Rule] -> WeightMap -> WeightMap
+calcMapsStep []               = id
+calcMapsStep (Rule t sev :xs) = (calcMapsStep xs).(Map.insertWith (+) t sev)
