1d467a5cede49dc7199b737197a55461cc0d401e
Justus Adam <development@justusadam.com>
2015-06-08 12:57:38 +0200

    Adding cors to request, failing

--- src/SchedulePlanner/Server.hs
+++ src/SchedulePlanner/Server.hs
@@ -14,27 +14,22 @@ Uses happstack and blaze to create a deployable service instance of this softwar
 module SchedulePlanner.Server (server, app) where
 
 
-import           Network.Wai.Handler.Warp (run)
-import           Network.Wai              (responseLBS, lazyRequestBody, Application, requestHeaders)
-import           Data.ByteString.Lazy     (ByteString)
-import           Network.HTTP.Types       (status200, HeaderName, Header)
-import qualified Data.ByteString          as BS (ByteString, intercalate)
+import           Network.Wai.Handler.Warp     (run)
+import           Network.Wai                  (responseLBS, lazyRequestBody,
+                                              Application, requestHeaders)
+import           Network.Wai.Middleware.Cors  (cors, CorsResourcePolicy(..),
+                                              simpleCorsResourcePolicy)
+import           Data.ByteString.Lazy         (ByteString)
+import           Network.HTTP.Types           (status200, HeaderName, Header, methodPost, methodOptions)
+import qualified Data.ByteString              as BS (ByteString, intercalate)
+import           Debug.Trace                  (traceShow)
 
 
-hOrigin :: HeaderName
-hOrigin = "Origin"
-
-
-hCrossOrigin :: HeaderName
-hCrossOrigin = "Access-Control-Allow-Origin"
-
-
-hAccessMethods :: HeaderName
-hAccessMethods = "Access-Control-Allow-Methods"
-
-
-crossOriginHeaderField :: [BS.ByteString] -> Header
-crossOriginHeaderField = (,) hCrossOrigin . BS.intercalate ", "
+applicationPolicy :: CorsResourcePolicy
+applicationPolicy = simpleCorsResourcePolicy
+  { corsOrigins = Just (["http://justus.science"], False)
+  , corsMethods = [ methodPost ]
+  }
 
 
 app :: (ByteString -> ByteString) -> Application
@@ -42,11 +37,9 @@ app app' request respond = do
   body <- lazyRequestBody request
   respond $ responseLBS status200 headers $ app' body
   where
-    rheaders = requestHeaders request
-    headers =
-      if elem hOrigin (map fst rheaders)
-        then [ crossOriginHeaderField [ "http://justus.science" ], (hAccessMethods, "POST") ]
-        else []
+    showIt :: Show a => a -> a
+    showIt a = traceShow a a
+    headers = []
 
 server :: Int -> (ByteString -> ByteString) -> IO ()
-server port = run port . app
+server port = run port . cors (\_ -> Just applicationPolicy) . app
